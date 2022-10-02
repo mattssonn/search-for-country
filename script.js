43308fb5f6157312895dbe52cabf546a36f87092
Emil Mattsson <emilmattsson14@gmail.com>
2022-10-02 21:27:17 +0200

    filter menu added. Styling almost finished

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 async function getData() {
-  const response = await fetch("https://restcountries.com/v3.1/all");
+  const response = await fetch(`https://restcountries.com/v3.1/name${name}`);
   const data = await response.json();
 
   const countryContainer = document.getElementById("country-container");
-
-  console.log(data);
 
   for (let i = 0; i < data.length; i++) {
     const div = document.createElement("section");
@@ -26,6 +24,31 @@ async function getData() {
     population.innerText = `Population: ${data[i].population}`;
     region.innerText = `Region: ${data[i].region}`;
     capitalCity.innerText = `Capital city: ${data[i].capital}`;
+    console.log(data[i].region);
   }
 }
 getData();
+
+// ----------------------------------------------------
+// ----------------------------------------------------
+// ----------------------------------------------------
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
