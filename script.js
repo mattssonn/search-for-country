cdbbbe0d7e463596f7b372c4066080d6e6483244
Emil Mattsson <emilmattsson14@gmail.com>
2022-10-05 23:22:45 +0200

    responsive website. Still needs work

--- script.js
+++ script.js
@@ -20,28 +20,36 @@ async function getData(continent) {
   countryContainer.innerHTML = "";
 
   for (let i = 0; i < data.length; i++) {
-    const div = document.createElement("section");
+    const div = document.createElement("div");
     const img = document.createElement("img");
     const imgContainer = document.createElement("div");
+    const textContainer = document.createElement("div");
+    div.className = "countryInfo";
     imgContainer.className = "img-container";
-    const countryName = document.createElement("h3");
+    textContainer.className = "text-container";
+    const countryName = document.createElement("h2");
     const population = document.createElement("p");
     const region = document.createElement("p");
     const capitalCity = document.createElement("p");
 
     countryContainer.appendChild(div);
     imgContainer.appendChild(img);
+    textContainer.appendChild(countryName);
+    textContainer.appendChild(population);
+    textContainer.appendChild(region);
+    textContainer.appendChild(capitalCity);
     div.appendChild(imgContainer);
-    div.appendChild(countryName);
-    div.appendChild(population);
-    div.appendChild(region);
-    div.appendChild(capitalCity);
+    div.appendChild(textContainer);
+    // div.appendChild(countryName);
+    // div.appendChild(population);
+    // div.appendChild(region);
+    // div.appendChild(capitalCity);
 
     img.src = data[i].flags.png;
     countryName.innerText = data[i].name.common;
-    population.innerHTML = `<span style="color: #000">Population:</span> ${data[i].population}`;
+    population.innerHTML = `<span style="color: #000; font-weight: 900">Population:</span> ${data[i].population}`;
     region.innerHTML = `<span style="color: #000; font-weight: 900">Region:</span> ${data[i].region}`;
-    capitalCity.innerHTML = `<span style="color: #000">Capital city</span>: ${data[i].capital}`;
+    capitalCity.innerHTML = `<span style="color: #000; font-weight: 900">Capital city</span>: ${data[i].capital}`;
   }
   toggle.addEventListener("click", function () {
     this.classList.toggle("bi-moon");
@@ -86,7 +94,7 @@ dropdowns.forEach((dropdown) => {
 
 getData();
 
-// input.addEventListener("keydown", () => {
-//   getData();
-// });
-input.addEventListener("keydown", getData);
+input.addEventListener("keydown", () => {
+  getData();
+});
+// input.addEventListener("keydown", getData);
